1d3bc0021ff539ad4a586d5b45371a2b6e5b702a
Rohithmunna31 <rohithkasna31@gmail.com>
2024-01-22 12:02:15 +0530

    added two routes and added an form

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 
+const path = require("path");
+
 const app = express();
 
 app.use("/home", (req, res, next) => {
   res.send("<h1> This is HOme page </h1>");
 });
 
-app.use("/about", (req, res, next) => {
-  res.send("<h1> This is About page </h1>");
+app.use("/add-product", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.use('/contacus', (req,res,next)=>{
+    res.send('<h1> This is about us  </h1>')
+})
+
+app.use("/success", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "./success.html"));
 });
 
 app.use((req, res, next) => {
